80558c56468d25d096aa2c0f26e2baeb51096218
18404715033 <965630447@qq.com>
2015-11-25 18:04:54 +0800

    Initial Commit

---
+++ the composition of digital/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  the composition of digital
+//
+//  Created by 李伯阳 on 15/11/25.
+//  Copyright © 2015年 李伯阳. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
